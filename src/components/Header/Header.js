52f4f13829f8d50b8b649c8460dea2fc6c215f58
Erik-NA7 <p.erikson.saragi@gmail.com>
2021-12-27 23:37:00 +0700

    working on responsiveness

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,37 +1,72 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
 import style from './Navbar.module.css'
 import { FaSearch } from 'react-icons/fa'
+import { FaBars } from 'react-icons/fa'
+
+const useWindowSize = () => {
+  const [ navbar, setNavBar] = useState(null)
+  const [ navtoglle, setNavToggle] = useState(null)
+  useEffect(() => {
+    const getWindowSize = () => {
+      if (window.innerWidth <= 1024) {
+        setNavBar(false)
+        setNavToggle(true)
+      }
+      if (window.innerWidth > 1024) {
+        setNavBar(true)
+        setNavToggle(false)
+      }
+    }
+    window.addEventListener("resize", getWindowSize)
+    return () => {
+      window.removeEventListener("resize", getWindowSize)
+    }
+  })
+  return [navbar, navtoglle] 
+}
 
 function Header(props) {
 
+  let [ showNavBar, showNavToggle ] = useWindowSize()
+
+  const [ toggleNav, setToggleNav ] = useState(false)
+
+  const onToggleNav = () => {
+    setToggleNav(!toggleNav)
+  }
+
   return (
     <header className={style['App-header']}>
-      <div className='container'>
+      <div className={`container ${style.nav}`}>
         <div className={style['nav-brand']}>
           <h2>Forum anak IT</h2>
-        </div>
-      <nav className={style.navbar}>
-        <div className={style['search-container']}>
-          <input className={style.searchbox} type='search' name='search' placeholder='Search'></input>
-          <FaSearch className={style['fa-search']}/>
-        </div>
-        <div className={style['nav-menu']}>
-          <div className={style['dropdown']}>
-            <button>Categories</button>
-            <div className={style['dropdown-content']}>
-              <menu>
-                <li>Linux</li>
-                <li>Windows</li>
-                <li>MAC OS</li>
-                <li>Android</li>
-                <li>iOS</li>
-              </menu>
-            </div>
+          { showNavToggle && <div className={style['nav-toggle']}>
+            <button onClick={onToggleNav}><FaBars/></button>
+          </div> }
+        </div>    
+        { (showNavBar || toggleNav) &&
+        <nav className={style.navbar}>
+          <div className={style['search-container']}>
+            <input className={style.searchbox} type='search' name='search' placeholder='Search'></input>
+            <i className={style['fa-search']}><FaSearch /></i>
           </div>
-          <button onClick={props.onLogin}>Login</button>
-          <button onClick={props.onRegister}>Register</button>
-        </div>
-      </nav>
+          <div className={style['nav-menu']}>
+            <div className={style['dropdown']}>
+              <button className={style.dropbtn}>Categories</button>
+              <div className={style['dropdown-content']}>
+                <menu>
+                  <li>Linux</li>
+                  <li>Windows</li>
+                  <li>MAC OS</li>
+                  <li>Android</li>
+                  <li>iOS</li>
+                </menu>
+              </div>
+            </div>
+            <button onClick={props.onLogin}>Login</button>
+            <button onClick={props.onRegister}>Register</button>
+          </div>
+        </nav> }
       </div>
     </header>
   )
